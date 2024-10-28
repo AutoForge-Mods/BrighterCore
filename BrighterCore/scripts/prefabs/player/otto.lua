03fc6434f176f4d3a12096e7b9d000d839cfa1b4
Chris <110122218+FadingLotus@users.noreply.github.com>
2024-10-28 11:05:23 +0000

    Add files via upload

--- BrighterCore/scripts/prefabs/player/otto.lua
+++ BrighterCore/scripts/prefabs/player/otto.lua
@@ -2,7 +2,7 @@ local ____lualib = require("lualib_bundle")
 local Map = ____lualib.Map
 local __TS__New = ____lualib.__TS__New
 local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["7"] = 1,["8"] = 1,["9"] = 2,["10"] = 2,["11"] = 3,["12"] = 3,["13"] = 4,["14"] = 4,["15"] = 5,["16"] = 5,["17"] = 7,["18"] = 9,["19"] = 13,["20"] = 17,["21"] = 21,["22"] = 23,["23"] = 25,["24"] = 25,["25"] = 25,["26"] = 25,["27"] = 25,["28"] = 25,["29"] = 25,["30"] = 25,["31"] = 25,["32"] = 25,["33"] = 36,["34"] = 38,["35"] = 38,["36"] = 38,["37"] = 38,["38"] = 38,["39"] = 36,["40"] = 42,["41"] = 46,["42"] = 47,["43"] = 48,["44"] = 52,["45"] = 53,["46"] = 53,["47"] = 54,["48"] = 54,["49"] = 54,["50"] = 53,["51"] = 55,["52"] = 55,["53"] = 55,["54"] = 53,["55"] = 56,["56"] = 56,["57"] = 56,["58"] = 53,["59"] = 57,["60"] = 57,["61"] = 57,["62"] = 53,["63"] = 58,["64"] = 58,["65"] = 58,["66"] = 53,["67"] = 59,["68"] = 59,["69"] = 59,["70"] = 53,["71"] = 60,["72"] = 60,["73"] = 60,["74"] = 53,["75"] = 61,["76"] = 61,["77"] = 61,["78"] = 53,["79"] = 62,["80"] = 62,["81"] = 62,["82"] = 53,["83"] = 63,["84"] = 63,["85"] = 63,["86"] = 53,["87"] = 53,["88"] = 52,["89"] = 67,["90"] = 69,["91"] = 70,["92"] = 72,["93"] = 72,["94"] = 72,["95"] = 72,["96"] = 72,["97"] = 72,["98"] = 72,["99"] = 72,["100"] = 80,["101"] = 82,["102"] = 83,["103"] = 84,["104"] = 85,["105"] = 86,["106"] = 88,["107"] = 89,["108"] = 90,["109"] = 91,["110"] = 92,["111"] = 93,["112"] = 94,["114"] = 97,["115"] = 98,["116"] = 99,["117"] = 101,["118"] = 101,["119"] = 101,["120"] = 102,["121"] = 103,["122"] = 104,["123"] = 105,["124"] = 106,["126"] = 108,["127"] = 109,["128"] = 109,["129"] = 109,["130"] = 109,["131"] = 110,["132"] = 111,["133"] = 113,["135"] = 114,["136"] = 114,["137"] = 115,["138"] = 116,["139"] = 117,["140"] = 118,["142"] = 114,["145"] = 121,["146"] = 122,["148"] = 101,["149"] = 101,["150"] = 126,["151"] = 126,["152"] = 126,["153"] = 127,["154"] = 128,["155"] = 129,["156"] = 130,["157"] = 131,["158"] = 132,["159"] = 133,["160"] = 134,["161"] = 136,["162"] = 137,["163"] = 138,["164"] = 136,["165"] = 126,["166"] = 126,["167"] = 72,["168"] = 72,["169"] = 144,["170"] = 144,["171"] = 144,["172"] = 144,["173"] = 144,["174"] = 144,["175"] = 151,["176"] = 156,["177"] = 157,["178"] = 158,["179"] = 160,["180"] = 160,["181"] = 160,["182"] = 160});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["7"] = 1,["8"] = 1,["9"] = 2,["10"] = 2,["11"] = 3,["12"] = 3,["13"] = 4,["14"] = 4,["15"] = 5,["16"] = 5,["17"] = 6,["18"] = 6,["19"] = 8,["20"] = 10,["21"] = 14,["22"] = 18,["23"] = 22,["24"] = 24,["25"] = 26,["26"] = 26,["27"] = 26,["28"] = 26,["29"] = 26,["30"] = 26,["31"] = 26,["32"] = 26,["33"] = 26,["34"] = 26,["35"] = 37,["36"] = 38,["37"] = 37,["38"] = 41,["39"] = 43,["40"] = 43,["41"] = 43,["42"] = 43,["43"] = 43,["44"] = 43,["45"] = 43,["46"] = 43,["47"] = 43,["48"] = 43,["49"] = 43,["50"] = 43,["51"] = 43,["52"] = 42,["53"] = 44,["54"] = 44,["55"] = 44,["56"] = 44,["57"] = 44,["58"] = 44,["59"] = 44,["60"] = 44,["61"] = 42,["62"] = 45,["63"] = 45,["64"] = 45,["65"] = 45,["66"] = 45,["67"] = 45,["68"] = 45,["69"] = 45,["70"] = 42,["71"] = 46,["72"] = 46,["73"] = 46,["74"] = 46,["75"] = 42,["76"] = 41,["77"] = 50,["78"] = 54,["79"] = 55,["80"] = 56,["81"] = 60,["82"] = 61,["83"] = 61,["84"] = 62,["85"] = 62,["86"] = 62,["87"] = 61,["88"] = 63,["89"] = 63,["90"] = 63,["91"] = 61,["92"] = 64,["93"] = 64,["94"] = 64,["95"] = 61,["96"] = 65,["97"] = 65,["98"] = 65,["99"] = 61,["100"] = 66,["101"] = 66,["102"] = 66,["103"] = 61,["104"] = 67,["105"] = 67,["106"] = 67,["107"] = 61,["108"] = 68,["109"] = 68,["110"] = 68,["111"] = 61,["112"] = 69,["113"] = 69,["114"] = 69,["115"] = 61,["116"] = 70,["117"] = 70,["118"] = 70,["119"] = 61,["120"] = 71,["121"] = 71,["122"] = 71,["123"] = 61,["124"] = 72,["125"] = 72,["126"] = 72,["127"] = 61,["128"] = 61,["129"] = 60,["130"] = 76,["131"] = 78,["132"] = 79,["133"] = 81,["134"] = 81,["135"] = 81,["136"] = 81,["137"] = 81,["138"] = 81,["139"] = 81,["140"] = 81,["141"] = 81,["142"] = 90,["143"] = 92,["144"] = 93,["145"] = 94,["146"] = 95,["147"] = 96,["148"] = 98,["149"] = 99,["150"] = 100,["151"] = 101,["152"] = 102,["153"] = 103,["154"] = 104,["156"] = 107,["157"] = 108,["158"] = 109,["159"] = 111,["160"] = 111,["161"] = 111,["162"] = 112,["163"] = 113,["164"] = 114,["165"] = 115,["166"] = 116,["168"] = 118,["169"] = 119,["170"] = 119,["171"] = 119,["172"] = 119,["173"] = 120,["174"] = 121,["175"] = 123,["177"] = 124,["178"] = 124,["179"] = 125,["180"] = 126,["181"] = 127,["182"] = 128,["184"] = 124,["187"] = 131,["188"] = 132,["190"] = 111,["191"] = 111,["192"] = 136,["193"] = 136,["194"] = 136,["195"] = 137,["196"] = 138,["197"] = 139,["198"] = 140,["199"] = 141,["200"] = 142,["201"] = 143,["202"] = 144,["203"] = 146,["204"] = 147,["205"] = 148,["206"] = 149,["207"] = 146,["208"] = 136,["209"] = 136,["210"] = 81,["211"] = 81,["212"] = 155,["213"] = 157,["214"] = 157,["215"] = 157,["216"] = 157,["217"] = 157,["218"] = 157,["219"] = 164,["220"] = 169,["221"] = 170,["222"] = 171,["223"] = 172,["224"] = 174,["225"] = 180,["226"] = 180,["227"] = 180,["228"] = 180,["229"] = 185,["230"] = 186,["231"] = 187,["232"] = 187,["233"] = 187,["234"] = 188,["235"] = 189,["236"] = 190,["237"] = 191,["238"] = 192,["239"] = 193,["240"] = 194,["242"] = 197,["243"] = 198,["244"] = 199,["245"] = 200,["247"] = 203,["248"] = 204,["249"] = 205,["250"] = 206,["251"] = 187,["252"] = 187,["253"] = 185});
 local ____exports = {}
 local ____input = require("components.input")
 local InputStateTypes = ____input.InputStateTypes
@@ -10,6 +10,8 @@ local ____location = require("components.location")
 local LayerTypes = ____location.LayerTypes
 local ____Sizes = require("Sizes")
 local Sizes = ____Sizes.Sizes
+local ____item = require("systems.item")
+local ItemSystem = ____item.ItemSystem
 local ____scheduler = require("systems.scheduler")
 local scheduler = ____scheduler.scheduler
 local ____world = require("world")
@@ -28,16 +30,51 @@ prefab:addPhysics({
     friction = 1,
     restitution = 0,
     layer = PhysicsLayers.PLAYER,
-    filter = PhysicsLayers.GROUND
+    filter = bit.bor(PhysicsLayers.GROUND, PhysicsLayers.ITEM)
 })
-prefab:addInventory({inventories = {{
-    size = 30,
-    features = bit.bor(
-        bit.bor(InventoryFeatures.Collect, InventoryFeatures.Deposit),
-        InventoryFeatures.Withdraw
-    )
-}}})
-prefab:addGatherer({range = 48})
+local function filterOutCurrencies(item)
+    return not ItemSystem.currencies:has(item.entityID)
+end
+prefab:addInventory({inventories = {
+    {
+        size = 40,
+        filter = filterOutCurrencies,
+        features = bit.bor(
+            bit.bor(
+                bit.bor(
+                    bit.bor(InventoryFeatures.Collect, InventoryFeatures.Deposit),
+                    InventoryFeatures.Withdraw
+                ),
+                InventoryFeatures.Craftable
+            ),
+            InventoryFeatures.Transfer
+        )
+    },
+    {
+        name = "ancient_tech",
+        size = 1,
+        filterPrefabs = {Prefab.getID("material.ancient_tech")},
+        features = bit.bor(
+            bit.bor(InventoryFeatures.Collect, InventoryFeatures.Deposit),
+            InventoryFeatures.Transfer
+        )
+    },
+    {
+        name = "void_matter",
+        size = 1,
+        filterPrefabs = {Prefab.getID("material.void_matter")},
+        features = bit.bor(
+            bit.bor(InventoryFeatures.Collect, InventoryFeatures.Deposit),
+            InventoryFeatures.Transfer
+        )
+    },
+    {
+        name = "trash",
+        size = 1,
+        features = bit.bor(InventoryFeatures.Deposit, InventoryFeatures.Withdraw)
+    }
+}})
+prefab:addGatherer({range = 64})
 prefab:addPlayer()
 prefab:addInteraction()
 prefab:addQuickbar({size = 4})
@@ -77,6 +114,10 @@ prefab:addStats({stats = __TS__New(
             Stat.new(2, 10)
         },
         {
+            "SPEED",
+            Stat.new(1, 2)
+        },
+        {
             "REACH",
             Stat.new(Sizes.TILE * 8, 999)
         },
@@ -97,6 +138,7 @@ prefab:addCombat({
     fx = {"action_fx.sword_slash", "action_fx.sword_slash2"},
     takeHitSfx = "event:/PLAYER/OTTO_TAKEHIT",
     deathSfx = "event:/PLAYER/OTTO_DEATH",
+    pushback = Vector.new(2, 0.5),
     onDeath = function(self, entity, killer)
         local realm = World:get(entity.location.realm)
         local apotheos = EntityManager.get(realm.apotheos)
@@ -161,11 +203,13 @@ prefab:addCombat({
                 entity.animation.onFinished:listen(function(____, name)
                     entity.combat.isInvincible = false
                     entity.input.locked = false
+                    return false
                 end)
             end
         )
     end
 })
+prefab:addEffect()
 prefab:addConsumer({
     isAutonomous = true,
     idlePower = 0,
@@ -176,8 +220,35 @@ prefab:addResearch({type = ResearcherType.Player, speed = 1})
 prefab:addAttachment()
 prefab:addCrafter()
 prefab:addArmory()
+prefab:addCosmetic()
+prefab:addMarker({category = MarkerCategory.Character, icon = "map_icons_otto", isTransient = true})
 prefab:addLight({
     intensity = 120,
     color = Color.new(200, 255, 255)
 })
+prefab.onCreate:listen(function(entity)
+    local entityID = entity.id
+    scheduler:add(
+        1,
+        function()
+            local entity = EntityManager.get(entityID)
+            local paletteMaterial = Renderer.getMaterialHandle("sprite_palette")
+            entity.render.material = paletteMaterial
+            local cosmeticComponent = entity.cosmetic
+            if cosmeticComponent.cosmetic:isEmpty() then
+                cosmeticComponent.cosmetic = HashString("Default")
+                cosmeticComponent.palette = HashString("palette_otto_default")
+            end
+            if not cosmetic.apply(entity, cosmeticComponent.cosmetic, cosmeticComponent.palette) then
+                cosmeticComponent.cosmetic = HashString("Default")
+                cosmeticComponent.palette = HashString("palette_otto_default")
+                cosmetic.apply(entity, cosmeticComponent.cosmetic, cosmeticComponent.palette)
+            end
+            local arm = EntityManager.get(entity.attachment:get("arm"))
+            arm.render.material = paletteMaterial
+            cosmetic.apply(arm, cosmeticComponent.cosmetic, cosmeticComponent.palette)
+            animator.setAssociation(arm, "otto_right_arm")
+        end
+    )
+end)
 return ____exports
